b46fee06c6d47c3455d37af51fb87ce94b178639
jypma <jan@ypmania.nl>
2015-03-20 13:30:11 +0100

    add support for multiple timer modes

--- inc/Timer.hpp
+++ inc/Timer.hpp
@@ -48,9 +48,6 @@ extern InterruptChain tm2ocrb;
 template <typename info>
 class TimerComparator {
 public:
-    inline void setTarget(typename info::value_t value) {
-        *info::ocr = value;
-    }
     inline InterruptChain &interrupt() const {
         return *info::handler;
     }
@@ -63,15 +60,61 @@ public:
     }
 };
 
+enum class NonPWMOutputMode: uint8_t {
+    disconnected = 0,
+    toggle_on_match = 1,
+    clear_on_match = 2,
+    set_on_match = 3
+};
+
 template <typename info>
+class NonPWMTimerComparator: public TimerComparator<info> {
+public:
+    /**
+     * Sets the pin output mode, i.e. what should happen to this comparator's linked
+     * pin whenever the comparator matches.
+     */
+    void output(NonPWMOutputMode mode) const {
+        *info::tccra = (*info::tccra & ~(info::output_mode_bitmask)) | (static_cast<uint8_t>(mode) << info::output_mode_bitstart);
+    }
+    /**
+     * Sets the target at which the next comparator match event is to take place.
+     * Takes effect immediately.
+     */
+    inline void setTarget(typename info::value_t value) {
+        *info::ocr = value;
+    }
+};
+
+enum class FastPWMOutputMode: uint8_t {
+    disconnected = 0,
+    connected = 2,
+    connected_inverting = 3
+};
+
+template <typename info>
+class FastPWMTimerComparator: public TimerComparator<info> {
+public:
+    /**
+     * Sets the pin output mode, i.e. what should happen to this comparator's linked
+     * pin whenever the comparator matches.
+     */
+    void output(FastPWMOutputMode mode) const {
+        *info::tccra = (*info::tccra & ~(info::output_mode_bitmask)) | (static_cast<uint8_t>(mode) << info::output_mode_bitstart);
+    }
+    /**
+     * Sets the target at which the next comparator match event is to take place.
+     * Takes effect at the start of the next timer run (i.e. after the next overflow).
+     */
+    inline void setTargetFromNextRun(typename info::value_t value) {
+        *info::ocr = value;
+    }
+};
+
+template <typename info, typename comparator_a_t, typename comparator_b_t>
 class Timer {
 public:
     typedef typename info::value_t value_t;
-protected:
-    void configureFastPWM(const typename info::prescaler_t prescaler) const {
-        AtomicScope::SEI _;
-        info::configureFastPWM(prescaler);
-    }
 public:
     inline InterruptChain &interruptOnOverflow() const {
         return *info::intHandler;
@@ -89,11 +132,11 @@ public:
     inline bool isOverflow() const {
         return *info::tifr & _BV(TOV0);
     }
-    inline TimerComparator<typename info::ComparatorA> comparatorA() const {
-        return TimerComparator<typename info::ComparatorA>();
+    inline comparator_a_t comparatorA() const {
+        return comparator_a_t();
     }
-    inline TimerComparator<typename info::ComparatorB> comparatorB() const {
-        return TimerComparator<typename info::ComparatorB>();
+    inline comparator_b_t comparatorB() const {
+        return comparator_b_t();
     }
 
     /** 8 for 8-bit timer, 16 for 16-bit timer */
@@ -152,16 +195,42 @@ template<> struct PrescalerMeta<IntPrescaler,IntPrescaler::_1024> {
     constexpr static uint8_t power2 = 10;
 };
 
-template <typename info, typename info::prescaler_t _prescaler>
-class PrescaledTimer : public Timer<info> {
+template <typename info, typename info::prescaler_t _prescaler, typename comparator_a_t, typename comparator_b_t>
+class PrescaledTimer : public Timer<info, comparator_a_t, comparator_b_t> {
     typedef PrescalerMeta<typename info::prescaler_t,_prescaler> Meta;
+protected:
+    inline PrescaledTimer() {}
 public:
-    PrescaledTimer() {
-        Timer<info>::configureFastPWM(prescaler);
-    }
-
     static constexpr typename info::prescaler_t prescaler = _prescaler;
     static constexpr uint8_t prescalerPower2 = Meta::power2;
+};
+
+/**
+ * In fast PWM mode, changes to the comparator values apply on the next timer run.
+ * However, hardware PWM can apply those comparator values.
+ */
+template <typename info, typename info::prescaler_t _prescaler>
+class FastPWMTimer: public PrescaledTimer<info, _prescaler, FastPWMTimerComparator<typename info::ComparatorA>, FastPWMTimerComparator<typename info::ComparatorB>> {
+public:
+    inline FastPWMTimer() {
+        info::configureFastPWM(_prescaler);
+    }
+    typedef void is_fastpwm;
+
+};
+
+/**
+ * In normal mode, changes to the comparator values apply immediately.
+ * However, no hardware PWM can be performed.
+ */
+template <typename info, typename info::prescaler_t _prescaler>
+class NormalTimer: public PrescaledTimer<info, _prescaler, NonPWMTimerComparator<typename info::ComparatorA>, NonPWMTimerComparator<typename info::ComparatorB>> {
+public:
+    inline NormalTimer() {
+        info::configureNormal(_prescaler);
+    }
+    typedef void is_normal;
+
 };
 
 struct Timer0Info {
@@ -179,8 +248,17 @@ struct Timer0Info {
     typedef uint8_t value_t;
     typedef ExtPrescaler prescaler_t;
 
+    inline static void configureNormal(prescaler_t p) {
+        // Existing settings for COM0A1 etc. should be kept
+        AtomicScope::SEI _;
+        *tccra &= ~_BV(WGM00);
+        *tccra &= ~_BV(WGM01); // Mode 0, normal
+        *tccrb = static_cast<uint8_t>(p);
+    }
+
     inline static void configureFastPWM(prescaler_t p) {
-        // Existing settings for COM0A1 etc. should be kept when configuring fast PWM.
+        // Existing settings for COM0A1 etc. should be kept
+        AtomicScope::SEI _;
         *tccra |= _BV(WGM00);
         *tccra |= _BV(WGM01); // Mode 3, count up to 0xFF
         *tccrb = static_cast<uint8_t>(p);
@@ -190,6 +268,7 @@ struct Timer0Info {
         typedef uint8_t value_t;
         static constexpr volatile uint8_t *timsk = &TIMSK0;
         static constexpr volatile uint8_t *tifr = &TIFR0;
+        static constexpr volatile uint8_t *tccra = &TCCR0A;
     };
 
     struct ComparatorA: public Comparator {
@@ -197,12 +276,17 @@ struct Timer0Info {
         static constexpr uint8_t timsk_bit = OCIE0A;
         static constexpr uint8_t tifr_bit = OCF0A;
         static constexpr InterruptChain* handler = &tm0ocra;
+        static constexpr uint8_t output_mode_bitmask = (1 << COM0A0) | (1 << COM0A1);
+        static constexpr uint8_t output_mode_bitstart = COM0A0;
+
     };
     struct ComparatorB: public Comparator {
         static constexpr volatile uint8_t *ocr = &OCR0B;
         static constexpr uint8_t timsk_bit = OCIE0B;
         static constexpr uint8_t tifr_bit = OCF0B;
         static constexpr InterruptChain* handler = &tm0ocrb;
+        static constexpr uint8_t output_mode_bitmask = (1 << COM0B0) | (1 << COM0B1);
+        static constexpr uint8_t output_mode_bitstart = COM0B0;
     };
 };
 
@@ -221,8 +305,17 @@ struct Timer1Info {
     typedef uint16_t value_t;
     typedef ExtPrescaler prescaler_t;
 
+    inline static void configureNormal(prescaler_t p) {
+        // Existing settings for COM0A1 etc. should be kept
+        AtomicScope::SEI _;
+        *tccra &= ~_BV(WGM10);
+        *tccra &= ~_BV(WGM11);
+        *tccrb = static_cast<uint8_t>(p);
+    }
+
     inline static void configureFastPWM(prescaler_t p) {
-        // Existing settings for COM0A1 etc. should be kept when configuring fast PWM.
+        // Existing settings for COM0A1 etc. should be kept
+        AtomicScope::SEI _;
         *tccra &= ~_BV(WGM10);
         *tccra |= _BV(WGM11);
         *tccrb = _BV(WGM13) | _BV(WGM12) | static_cast<uint8_t>(p);
@@ -233,6 +326,7 @@ struct Timer1Info {
         typedef uint16_t value_t;
         static constexpr volatile uint8_t *timsk = &TIMSK1;
         static constexpr volatile uint8_t *tifr = &TIFR1;
+        static constexpr volatile uint8_t *tccra = &TCCR1A;
     };
 
     struct ComparatorA: public Comparator {
@@ -240,12 +334,16 @@ struct Timer1Info {
         static constexpr uint8_t timsk_bit = OCIE1A;
         static constexpr uint8_t tifr_bit = OCF1A;
         static constexpr InterruptChain* handler = &tm1ocra;
+        static constexpr uint8_t output_mode_bitmask = (1 << COM1A0) | (1 << COM1A1);
+        static constexpr uint8_t output_mode_bitstart = COM1A0;
     };
     struct ComparatorB: public Comparator {
         static constexpr volatile uint16_t *ocr = &OCR1B;
         static constexpr uint8_t timsk_bit = OCIE1B;
         static constexpr uint8_t tifr_bit = OCF1B;
         static constexpr InterruptChain* handler = &tm1ocrb;
+        static constexpr uint8_t output_mode_bitmask = (1 << COM1B0) | (1 << COM1B1);
+        static constexpr uint8_t output_mode_bitstart = COM1B0;
     };
 };
 
@@ -263,8 +361,17 @@ struct Timer2Info {
     typedef uint8_t value_t;
     typedef IntPrescaler prescaler_t;
 
+    inline static void configureNormal(prescaler_t p) {
+        // Existing settings for COM0A1 etc. should be kept
+        AtomicScope::SEI _;
+        *tccra &= ~_BV(WGM20);
+        *tccra &= ~_BV(WGM21); // Mode 0, normal
+        *tccrb = static_cast<uint8_t>(p);
+    }
+
     inline static void configureFastPWM(prescaler_t p) {
-        // Existing settings for COM0A1 etc. should be kept when configuring fast PWM.
+        // Existing settings for COM0A1 etc. should be kept
+        AtomicScope::SEI _;
         *tccra |= _BV(WGM20);
         *tccra |= _BV(WGM21); // Mode 3, count up to 0xFF
         *tccrb = static_cast<uint8_t>(p);
@@ -274,6 +381,7 @@ struct Timer2Info {
         typedef uint8_t value_t;
         static constexpr volatile uint8_t *timsk = &TIMSK2;
         static constexpr volatile uint8_t *tifr = &TIFR2;
+        static constexpr volatile uint8_t *tccra = &TCCR2A;
     };
 
     struct ComparatorA: public Comparator {
@@ -281,17 +389,24 @@ struct Timer2Info {
         static constexpr uint8_t timsk_bit = OCIE2A;
         static constexpr uint8_t tifr_bit = OCF2A;
         static constexpr InterruptChain* handler = &tm2ocra;
+        static constexpr uint8_t output_mode_bitmask = (1 << COM2A0) | (1 << COM2A1);
+        static constexpr uint8_t output_mode_bitstart = COM2A0;
     };
     struct ComparatorB: public Comparator {
         static constexpr volatile uint8_t *ocr = &OCR2B;
         static constexpr uint8_t timsk_bit = OCIE2B;
         static constexpr uint8_t tifr_bit = OCF2B;
         static constexpr InterruptChain* handler = &tm2ocrb;
+        static constexpr uint8_t output_mode_bitmask = (1 << COM2B0) | (1 << COM2B1);
+        static constexpr uint8_t output_mode_bitstart = COM2B0;
     };
 };
 
-template <ExtPrescaler prescaler> using Timer0 = PrescaledTimer<Timer0Info,prescaler>;
-template <ExtPrescaler prescaler> using Timer1 = PrescaledTimer<Timer1Info,prescaler>;
-template <IntPrescaler prescaler> using Timer2 = PrescaledTimer<Timer2Info,prescaler>;
+template <ExtPrescaler prescaler> using Timer0_Normal = NormalTimer<Timer0Info,prescaler>;
+template <ExtPrescaler prescaler> using Timer1_Normal = NormalTimer<Timer1Info,prescaler>;
+template <IntPrescaler prescaler> using Timer2_Normal = NormalTimer<Timer2Info,prescaler>;
+template <ExtPrescaler prescaler> using Timer0_FastPWM = FastPWMTimer<Timer0Info,prescaler>;
+template <ExtPrescaler prescaler> using Timer1_FastPWM = FastPWMTimer<Timer1Info,prescaler>;
+template <IntPrescaler prescaler> using Timer2_FastPWM = FastPWMTimer<Timer2Info,prescaler>;
 
 #endif /* TIMERS_HPP_ */
