159360959acc51d5e89b5f8e57bba1f769e60a5d
jypma <jan@ypmania.nl>
2016-10-09 13:45:08 +0200

    now works with 6x sensors

--- apps/DHTCalib/DHTCalib.hpp
+++ apps/DHTCalib/DHTCalib.hpp
@@ -25,25 +25,25 @@ struct DHTCalib {
     Usart0 usart0 = { 57600 };
     auto_var(pinTX, PinPD1<250>(usart0));
     auto_var(timer0, Timer0::withPrescaler<64>::inNormalMode());
-    auto_var(timer1, Timer1::withPrescaler<64>::inNormalMode());
+    auto_var(timer1, Timer1::lowByte::withPrescaler<64>::inNormalMode());
     auto_var(timer2, Timer2::withPrescaler<64>::inNormalMode());
     auto_var(rt, realTimer(timer0));
     auto_var(nextMeasurement, deadline(rt, 2_s));
 
-    auto_var(pinDHTPower, ArduinoPinD4());
+    auto_var(pinDHTPower, ArduinoPinD13());
     auto_var(pinDHT0, ArduinoPinA1().withInterruptOnChange());
     auto_var(pinDHT1, ArduinoPinA2().withInterruptOnChange());
     auto_var(pinDHT2, ArduinoPinA3().withInterruptOnChange());
-    //auto_var(pinDHT3, ArduinoPinA3().withInterrupt());
-    //auto_var(pinDHT4, ArduinoPinD13().withInterrupt());
-    //auto_var(pinDHT5, ArduinoPinD12().withInterrupt());
+    auto_var(pinDHT3, ArduinoPinA4().withInterrupt());
+    auto_var(pinDHT4, ArduinoPinD3());
+    auto_var(pinDHT5, ArduinoPinD4().withInterrupt());
 
     auto_var(dht0, DHT22(pinDHT0, pinDHTPower, timer0.comparatorA(), rt));
     auto_var(dht1, DHT22(pinDHT1, pinDHTPower, timer0.comparatorB(), rt));
     auto_var(dht2, DHT11(pinDHT2, pinDHTPower, timer1.comparatorA(), rt));
-    //auto_var(dht3, DHT11(pinDHT3, pinDHTPower, timer1.comparatorB(), rt));
-    //auto_var(dht4, DHT11(pinDHT4, pinDHTPower, timer2.comparatorA(), rt));
-    //auto_var(dht5, DHT11(pinDHT5, pinDHTPower, timer2.comparatorB(), rt));
+    auto_var(dht3, DHT11(pinDHT3, pinDHTPower, timer1.comparatorB(), rt));
+    auto_var(dht4, DHT11(pinDHT4, pinDHTPower, timer2.comparatorA(), rt));
+    auto_var(dht5, DHT11(pinDHT5, pinDHTPower, timer2.comparatorB(), rt));
 
     /*
     volatile uint8_t changes = 0;
@@ -52,32 +52,67 @@ struct DHTCalib {
     }
     */
 
+    typedef Delegate<This, decltype(dht4), &This::dht4,
+			Delegate<This, decltype(dht5), &This::dht5>> Others;
+
     typedef Delegate<This, decltype(pinTX), &This::pinTX,
             Delegate<This, decltype(rt), &This::rt,
 			Delegate<This, decltype(dht0), &This::dht0,
 			Delegate<This, decltype(dht1), &This::dht1,
-			Delegate<This, decltype(dht2), &This::dht2
-			>>>>> Handlers;
+			Delegate<This, decltype(dht2), &This::dht2,
+			Delegate<This, decltype(dht3), &This::dht3,
+			Others
+			>>>>>> Handlers;
 
     void measure() {
-        log::debug(F("Measuring DHT0"));
-        dht0.measure();
-        while (dht0.isMeasuring()) {
-        //	Logging::printTimings();
-        //	log::flush();
-        	dht0.loop();
+    	constexpr uint8_t MAX = 10;
+
+        //log::debug(F("Measuring DHT0"));
+        for (uint8_t i = 0; i < MAX; i++) {
+            dht0.measure();
+            while (dht0.isMeasuring()) {
+            	dht0.loop();
+            }
+        	if (dht0.getTemperature().isDefined()) break;
         }
 
-        log::debug(F("Measuring DHT1"));
-        dht1.measure();
-        while (dht1.isMeasuring()) {
-        	//log::debug(dec(pls));
-        	//log::debug(dec(HAL::Atmel::PinChangeVectorOnChange<HAL::Atmel::Info::PCInt1Info, 1 << 0>::ints));
-        	//log::debug(dec(HAL::Atmel::PinChangeVectorOnChange<HAL::Atmel::Info::PCInt1Info, 1 << 1>::ints));
-        	//log::debug(dec(HAL::Atmel::PinChangeVectorOnChange<HAL::Atmel::Info::PCInt1Info, 1 << 2>::ints));
-        	//log::flush();
-            //Logging::printTimings();
-        	dht1.loop();
+        //log::debug(F("Measuring DHT1"));
+        for (uint8_t i = 0; i < MAX; i++) {
+			dht1.measure();
+			while (dht1.isMeasuring()) {
+				dht1.loop();
+			}
+			if (dht1.getTemperature().isDefined()) break;
+		}
+
+		//log::debug(F("Measuring DHT2"));
+        for (uint8_t i = 0; i < MAX; i++) {
+			dht2.measure();
+			while (dht2.isMeasuring()) {
+				dht2.loop();
+			}
+			if (dht2.getTemperature().isDefined()) break;
+		}
+
+        //log::debug(F("Measuring DHT3"));
+        for (uint8_t i = 0; i < MAX; i++) {
+			dht3.measure();
+			while (dht3.isMeasuring()) {
+				dht3.loop();
+			}
+			if (dht3.getTemperature().isDefined()) break;
+		}
+
+        //log::debug(F("Measuring DHT4"));
+        dht4.measure();
+        while (dht4.isMeasuring()) {
+        	dht4.loop();
+        }
+
+        //log::debug(F("Measuring DHT5"));
+        dht5.measure();
+        while (dht5.isMeasuring()) {
+        	dht5.loop();
         }
     }
 
@@ -100,10 +135,10 @@ struct DHTCalib {
     	//pinTX.write(dec(pls), '/', dec(debugTimingsCount), endl);
         dht0.loop();
         dht1.loop();
-        //dht2.loop();
-        //dht3.loop();
-        //dht4.loop();
-        //dht5.loop();
+        dht2.loop();
+        dht3.loop();
+        dht4.loop();
+        dht5.loop();
         pinTX.flush();
         //log::debug(F("changes: "), dec(changes), F(", high: "), '0' + pinDHT1.isHigh(), F(", PCICR="), dec(PCICR));
         Logging::printTimings();
@@ -113,7 +148,16 @@ struct DHTCalib {
         if (nextMeasurement.isNow()) {
             measure();
             log::debug(F("DHT0: h="), dec(dht0.getHumidity()), F(" t="), dec(dht0.getTemperature()));
+            pinTX.flush();
             log::debug(F("DHT1: h="), dec(dht1.getHumidity()), F(" t="), dec(dht1.getTemperature()));
+            pinTX.flush();
+            log::debug(F("DHT2: h="), dec(dht2.getHumidity()), F(" t="), dec(dht2.getTemperature()));
+            pinTX.flush();
+            log::debug(F("DHT3: h="), dec(dht3.getHumidity()), F(" t="), dec(dht3.getTemperature()));
+            pinTX.flush();
+            log::debug(F("DHT4: h="), dec(dht4.getHumidity()), F(" t="), dec(dht4.getTemperature()));
+            pinTX.flush();
+            log::debug(F("DHT5: h="), dec(dht5.getHumidity()), F(" t="), dec(dht5.getTemperature()));
         //    pinTX.flush();
             nextMeasurement.schedule();
         }
